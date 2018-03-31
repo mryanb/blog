43d6cff295cfd171d9bfd1312c4bcc165d0bfbf7
Ryan Bantz <ryan@rykelabs.com>
2018-03-31 15:34:06 -0500

    Updated page

--- source/index.blade.php
+++ source/index.blade.php
@@ -4,11 +4,11 @@
 <div class="container">
     <div class="row cols-sm-12 cols-md-10">
         <div class="col-md-offset-1">
-            <div class="profile-image"></div>
+            <img src="/assets/images/" class="section media">
             <h1>Hey, I'm Ryan Bantz</h1>
             <h2>B2B SaaS Bootstrapper</h2>
 
-            <p>I'm a problem solver, a developer, and people person. I enjoy creating web applications and the challenge of getting them out into the wild.</p>
+            <p>I'm a problem solver, a developer, and people person. I enjoy creating web applications and the challenge of getting them out into the wild. Indoor rock climbing is something I enjoy.</p>
             <p>Currently, I'm the CEO at ChargeOver, working to build a SaaS company that can be great!</p>
         </div>
     </div>
