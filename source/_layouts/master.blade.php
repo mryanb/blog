aa0ee50c002fa7d907981f774525bb08dbbadcb6
Ryan Bantz <ryan@rykelabs.com>
2018-03-31 15:26:50 -0500

    first commit

---
+++ source/_layouts/master.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <link rel="stylesheet" href="/assets/css/main.css">
+    </head>
+    <body>
+        @yield('body')
+    </body>
+    <script type="text/javascript" src="/assets/js/main.js"></script>
+</html>
