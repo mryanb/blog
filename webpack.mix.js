9f8ada8eb7e311e53833fbbe6ba900860e04119e
Ryan Bantz <ryan@rykelabs.com>
2018-07-13 15:08:01 -0500

    #6 - Added bootstrap via yarm and laravel mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,10 @@
 let mix = require('laravel-mix');
 let build = require('./tasks/build.js');
 
+let ImageminPlugin = require('imagemin-webpack-plugin').default;
+let CopyWebpackPlugin = require('copy-webpack-plugin');
+let imageminMozjpeg = require('imagemin-mozjpeg');
+
 mix.disableSuccessNotifications();
 mix.setPublicPath('source/assets/');
 mix.webpackConfig({
@@ -8,9 +12,21 @@ mix.webpackConfig({
         build.jigsaw,
         build.browserSync(),
         build.watch(['source/**/*.md', 'source/**/*.php', 'source/**/*.scss']),
+        new CopyWebpackPlugin([{
+            from: 'source/_assets/images',
+            to: 'images', // Laravel mix will place this in 'assets/images'
+        }]),
+        new ImageminPlugin({
+            test: /\.(jpe?g|png|gif|svg)$/i,
+            plugins: [
+                imageminMozjpeg({
+                    quality: 80,
+                })
+            ]
+        }),
     ]
 });
 
-mix.js('source/_assets/js/main.js', 'js')
+mix.js(['source/_assets/js/main.js', 'node_modules/bootstrap/dist/js/bootstrap.js'], 'js')
     .sass('source/_assets/sass/main.scss', 'css/main.css')
     .version();
