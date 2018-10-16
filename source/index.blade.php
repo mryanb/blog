7e3a5149b6cac0d727b8d2f298d574428dcec44e
Ryan Bantz <ryan@rykelabs.com>
2018-10-15 22:21:30 -0500

    Updates

--- source/index.blade.php
+++ source/index.blade.php
@@ -2,10 +2,14 @@
 
 @section('body')
 <div class="container">
-    <div class="row cols-sm-12 cols-md-10">
-        <div class="col-md-offset-1">
+    <div class="row cols-sm-12 cols-md-10 header">
+        <div class="col-md-offset-1 col-md-4">
+            <a href="https://placeholder.com"><img src="https://via.placeholder.com/425x275"></a>
             <!-- <img src="/assets/images/" class="section media"> -->
-            <h1>ryanbantz.com</h1>
+        </div>
+        <div class="col-md-6">
+            <h1>Hi, I'm Ryan!></h1>
+            <p>This is a paragraph that I will write some stuff in.</p>
         </div>
     </div>
 </div>
